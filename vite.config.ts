290916adab93558a78ee21077b7a6ffb9e26bbcd
Sam Hillier <sam@hillier.uk>
2023-08-30 18:28:43 +0100

    Deploy gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/fyp23-24/'
+  }
+
+  return config
 })
