50159716b539a417ac095120a144b864fa511dce
Sam Hillier <sam@hillier.uk>
2024-02-10 02:46:22 +0000

    Add Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Nav from './components/Nav';
+import Footer from './components/Footer'
 function App() {
   return (
     <section className="App">
       <h1>Explore NFTs</h1>
       <Nav />
+      <Footer />
     </section>
   );
 }
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,70 @@
+import styles from './Footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXTwitter, faLinkedin, faGithub, faYoutube, faDiscord } from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () => {
+  // Social Media Links
+  const socialMediaLinks = [
+    { name: 'Twitter', url: 'https://x.com/Zyphaex', icon: faXTwitter },
+    { name: 'LinkedIn', url: 'https://linkedin.com/in/samhillier/', icon: faLinkedin },
+    { name: 'GitHub', url: 'https://github.com/Zyphaex', icon: faGithub },
+    { name: 'YouTube', url: 'https://youtube.com/@Zyphaex', icon: faYoutube },
+    { name: 'Discord', url: 'https://discord.com/invite/XfZHVfPr9C', icon: faDiscord },
+  ];
+
+  // Footer Links
+  const footerLinks = [
+    { name: 'About', href: 'about' },
+    { name: 'Contact', href: 'contact' },
+    { name: 'Terms & Conditions', href: 'terms' },
+    { name: 'Privacy Policy', href: 'privacy' },
+  ];
+
+  const renderSocialMediaLinks = () => {
+    return (
+      <ul className={styles.footerSocial} aria-label="Social Media Links">
+        {socialMediaLinks.map((socialLink) => (
+          <li key={socialLink.name}>
+            <a
+              href={socialLink.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label={socialLink.name}
+            >
+              <FontAwesomeIcon icon={socialLink.icon} />
+            </a>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const renderFooterLinks = () => {
+    return (
+      <ul className={styles.footerLinks} aria-label="Footer Navigation">
+        {footerLinks.map((link, index) => (
+          <li key={index}>
+            <a href={`#${link.href}`}>{link.name}</a>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  return (
+    <footer>
+      <section className={styles.footerContainer}>
+        <a className={styles.footerLogo} href="https://github.com/Zyphaex/fyp2024" target="_blank">
+          <img src="src\assets\images\ntu-logo-big.webp" alt="Nottingham Trent University" />
+        </a>
+        {renderFooterLinks()}
+      </section>
+      <section className={styles.footerContainer}>
+        {renderSocialMediaLinks()}
+        <p className={styles.footerLegal}>&copy; {new Date().getFullYear()} Sam Hillier. All Rights Reserved.</p>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
