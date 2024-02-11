2c1ae0c3f211fd14e386cdfa7ceb97e9bf9d45aa
Sam Hillier <sam@hillier.uk>
2024-02-11 08:23:14 +0000

    Add Nav hamburger

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,27 +1,49 @@
+import { useState } from 'react';
 import styles from './Nav.module.css';
 
 const navLinks = [
-    { name: 'Avatars', href: '#avatars' },
-    { name: 'Metaverse', href: '#metaverse' },
-    { name: 'Art', href: '#art' },
-    { name: 'Memberships', href: '#memberships' }
-  ];
+  { name: 'Avatars', href: '#avatars' },
+  { name: 'Metaverse', href: '#metaverse' },
+  { name: 'Art', href: '#art' },
+  { name: 'Memberships', href: '#memberships' }
+];
 
-  const Nav = ({ links = navLinks }) => {
-    return (
-      <nav className={styles.nav}>
-        <ul className={styles.navLinks}>
-          {links.map((link, index) => (
-            <li key={index}>
-              <a href={link.href}>{link.name}</a>
-            </li>
-          ))}
-        </ul>
+const Nav = ({ links = navLinks }) => {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavVisible(!isNavVisible);
+  };
+
+  return (
+    <nav>
+      <div className={styles.mobileTopRow}>
+        <div className={styles.hamburger} onClick={toggleNav}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
         <div className={styles.searchContainer}>
           <input type="text" className={styles.searchInput} placeholder="Search" />
         </div>
-      </nav>
-    );
+      </div>
+      <div className={styles.hamburger} onClick={toggleNav}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={`${styles.navLinks} ${isNavVisible ? styles.showNav : ''}`}>
+        {links.map((link, index) => (
+          <li key={index}>
+            <a href={link.href}>{link.name}</a>
+          </li>
+        ))}
+      </ul>
+      <div className={styles.searchContainer}>
+        <input type="text" className={styles.searchInput} placeholder="Search" />
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
