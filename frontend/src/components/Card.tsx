e423bee795750a372e619ebaf2f4725fa0c17522
Sam Hillier <sam@hillier.uk>
2024-02-16 11:51:41 +0000

    Add collections

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,16 +2,18 @@ import styles from './Card.module.css';
 
 interface Card {
   name: string;
+  collection: string;
   type: string;
   imageUrl: string;
   link: string;
 }
 
-function Card({ name, type, imageUrl, link }: Card) {
+function Card({ name, collection, type, imageUrl, link }: Card) {
   return (
     <a href={link} target="_blank" rel="noopener noreferrer">
       <div className={styles.card}>
         <h3>{name}</h3>
+        <h4>{collection}</h4>
         <p>{type}</p>
         <img src={imageUrl} alt={name} />
       </div>
